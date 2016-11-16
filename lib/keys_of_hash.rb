00daa2dacbaf797a8f7e7b494093250c65553a7c
p sc <patty.stacruz@gmail.com>
2016-11-16 19:38:50 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    arr = []
+    arguments.each do |argument|
+      keys.each do |animal|
+        if fetch(animal) == argument
+          arr << animal
+        end
+      end
+    end
+    return arr
   end
 end
